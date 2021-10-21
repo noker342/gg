861e273e0a84b9cde99359d36efc968845573a36
compus <sdfs@sd.ru>
2021-10-21 19:59:34 +0300

    commit

--- new.py
+++ new.py
@@ -12,19 +12,36 @@ class Cell:
     def __init__(self, x, y, width, height):
         self.x, self.y = x, y
         self.width, self.height = width, height
-        self.true = True
-        self.color = (255, 255, 255)
+        self.alive = False
+        self.colors = [(0, 0, 0), (255, 255, 255)]
 
     def show(self):
-        if self.true:
-            pygame.draw.rect(screen, self.color, (20, 20, 40, 40))
+        if self.alive:
+            pygame.draw.rect(screen, self.colors[self.alive], (self.x, self.y, self.width, self.height))
 
     def dying(self):
+        self.alive = not self.alive
 
 
-c = Cell(x, y, width, height)
+class Background:
+    def __init__(self, width, height):
+        self.cells = []
+        self.width = width
+        self.height = height
+        self.square_size = 20
+        for i in range(self.height // self.square_size):
+            self.cells.append([])
+            for j in range(self.width // self.square_size):
+                self.cells[-1].append(Cell(j * 20, i * 20, self.square_size, self.square_size))
+
+    def draw_cells(self):
+        for i in range(self.height // self.square_size):
+            pygame.draw.line(screen, (255, 255, 255), (i * 20, 0), (1, 1))
+            for j in range(self.width // self.square_size):
+                pygame.draw.line(screen, (255, 255, 255), (0, j * 20), (1, 1))
 
 
+b = Background(width, height)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,5 +49,5 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-    c.show()
+    b.draw_cells()
     pygame.display.flip()
