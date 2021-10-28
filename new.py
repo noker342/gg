4a4e6d2c2bc1bf402922ffffae2533303f87dc88
noker342 <71784957+noker342@users.noreply.github.com>
2021-10-28 19:57:58 +0300

    cells_dead

--- new.py
+++ new.py
@@ -26,6 +26,7 @@ class Cell:
 class Background:
     def __init__(self, width, height):
         self.cells = []
+        self.cells_dead = []
         self.width = width
         self.height = height
         self.square_size = 20
@@ -36,12 +37,51 @@ class Background:
 
     def draw_cells(self):
         for i in range(self.height // self.square_size):
-            pygame.draw.line(screen, (255, 255, 255), (i * 20, 0), (1, 1))
             for j in range(self.width // self.square_size):
-                pygame.draw.line(screen, (255, 255, 255), (0, j * 20), (1, 1))
+                self.cells[i][j].show()
 
+    def cell_count(self, x, y):
+        score = 0
+        if 0 <= y - 1 < 54 and 0 <= x - 1 < 96:
+            if self.cells[y - 1][x - 1].alive:
+                score += 1
+        if 0 <= y - 1 < 54 and 0 <= x < 96:
+            if self.cells[y - 1][x].alive:
+                score += 1
+        if 0 <= y - 1 < 54 and 0 <= x + 1 < 96:
+            if self.cells[y - 1][x + 1].alive:
+                score += 1
+        if 0 <= y < 54 and 0 <= x - 1 < 96:
+            if self.cells[y][x - 1].alive:
+                score += 1
+        if 0 <= y < 54 and 0 <= x + 1 < 96:
+            if self.cells[y][x + 1].alive:
+                score += 1
+        if 0 <= y + 1 < 54 and 0 <= x - 1 < 96:
+            if self.cells[y + 1][x - 1].alive:
+                score += 1
+        if 0 <= y + 1 < 54 and 0 <= x < 96:
+            if self.cells[y + 1][x].alive:
+                score += 1
+        if 0 <= y + 1 < 54 and 0 <= x + 1 < 96:
+            if self.cells[y + 1][x + 1].alive:
+                score += 1
+        return score
+
+    def dead_cells(self):
+        for i in range(self.height // self.square_size):
+            self.cells_dead.append([])
+            for j in range(self.width // self.square_size):
+                if not b.cells[i][j].dying():
+                    self.cells[-1].append(Cell(j * 20, i * 20, self.square_size, self.square_size))
+        print(self.cells)
 
 b = Background(width, height)
+# b.cells[34][28].dying()
+# b.cells[2][43].dying()
+# b.cells[51][51].dying()
+# b.cells[52][94].dying()
+b.cells[53][95].dying()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -50,4 +90,6 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     b.draw_cells()
+    b.cell_count(95, 54)
+    b.dead_cells()
     pygame.display.flip()
