7c3e54d7d13b4d4f3bfa71c1ff8e456f68b2ebe3
noker342 <71784957+noker342@users.noreply.github.com>
2021-11-11 19:55:50 +0300

    changes

---
+++ jest.py
@@ -0,0 +1,109 @@
+import pygame
+
+pygame.init()
+width = 1920
+height = 1080
+screen = pygame.display.set_mode((width, height))
+x, y = 0, 0
+running = True
+clock = pygame.time.Clock()
+
+
+class Cell:
+    def __init__(self, x, y, width, height):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        self.alive = False
+        self.colors = [(0, 0, 0), (255, 255, 255)]
+
+    def show(self):
+        if self.alive:
+            pygame.draw.rect(screen, self.colors[self.alive], (self.x, self.y, self.width, self.height))
+
+    def dying(self):
+        self.alive = not self.alive
+
+
+class Background:
+    def __init__(self, width, height):
+        self.cells = []
+        self.width = width
+        self.height = height
+        self.square_size = 20
+        for i in range(self.height // self.square_size):
+            self.cells.append([])
+            for j in range(self.width // self.square_size):
+                self.cells[-1].append(Cell(j * 20, i * 20, self.square_size, self.square_size))
+
+    def draw_cells(self):
+        for i in range(self.height // self.square_size):
+            for j in range(self.width // self.square_size):
+                self.cells[i][j].show()
+
+    def cell_count(self, x, y):
+        score = 0
+        if 0 <= y - 1 < self.height // self.square_size and 0 <= x - 1 < self.width // self.square_size:
+            if self.cells[y - 1][x - 1].alive:
+                score += 1
+        if 0 <= y - 1 < self.height//20 and 0 <= x < self.width // self.square_size:
+            if self.cells[y - 1][x].alive:
+                score += 1
+        if 0 <= y - 1 < self.height//20 and 0 <= x + 1 < self.width // self.square_size:
+            if self.cells[y - 1][x + 1].alive:
+                score += 1
+        if 0 <= y < self.height//20 and 0 <= x - 1 < self.width // self.square_size:
+            if self.cells[y][x - 1].alive:
+                score += 1
+        if 0 <= y < self.height//20 and 0 <= x + 1 < self.width // self.square_size:
+            if self.cells[y][x + 1].alive:
+                score += 1
+        if 0 <= y + 1 < self.height//20 and 0 <= x - 1 < self.width // self.square_size:
+            if self.cells[y + 1][x - 1].alive:
+                score += 1
+        if 0 <= y + 1 < self.height//20 and 0 <= x < self.width // self.square_size:
+            if self.cells[y + 1][x].alive:
+                score += 1
+        if 0 <= y + 1 < self.height//20 and 0 <= x + 1 < self.width // self.square_size:
+            if self.cells[y + 1][x + 1].alive:
+                score += 1
+        return score
+
+    def next_step(self):
+        cells_dead = []
+        for i in range(self.height // self.square_size):
+            cells_dead.append([])
+            for j in range(self.width // self.square_size):
+                cells_dead[-1].append(Cell(j * 20, i * 20, self.square_size, self.square_size))
+
+        for y in range(self.height // self.square_size):
+            for x in range(self.width // self.square_size):
+                count = self.cell_count(x, y)
+                if count < 2:
+                    pass
+                if count == 2 and self.cells[y][x].alive:
+                    cells_dead[y][x].dying()
+                if count == 3:
+                    cells_dead[y][x].dying()
+                if count > 3:
+                    pass
+        self.cells.clear()
+        self.cells = cells_dead.copy()
+
+
+b = Background(width, height)
+for i in range(54):
+    b.cells[i][48].dying()
+for j in range(93):
+    b.cells[27][j].dying()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    screen.fill((0, 0, 0))
+    b.draw_cells()
+    b.next_step()
+    pygame.display.flip()
+    clock.tick(3)
